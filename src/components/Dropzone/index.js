7e4eadb7450c5a465178d58d35dbc4a794a71407
ali <ali.hammami.pro@gmail.com>
2019-05-18 02:41:52 +0200

    send file to the api

--- src/components/Dropzone/index.js
+++ src/components/Dropzone/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './dropzone.scss';
 
 class Dropzone extends Component {
@@ -7,6 +9,7 @@ class Dropzone extends Component {
     this.state = {
       highlight: false,
       fileName: '',
+      message: '',
     };
     this.onDragOver = this.onDragOver.bind(this);
     this.onDragLeave = this.onDragLeave.bind(this);
@@ -32,28 +35,50 @@ class Dropzone extends Component {
         highlight: false,
       });
     }
+    const file = this.state.file;
+    const formData = new FormData();
+
+    formData.append('file', file);
+    axios({
+      url: 'https://fhirtest.uhn.ca/baseDstu3/Binary',
+      method: 'POST',
+      data: formData,
+    }).then((res) => {
+      console.log(res);
+      this.setState({ message: 'File has been successfully uploaded' });
+    }).catch((err) => {
+      console.log(err.response);
+    });
   }
 
   render() {
-    const { fileName, highlight } = this.state;
+    const { fileName, highlight, message } = this.state;
     const noFileName = fileName === '';
     return (
-      <div
-        className={`dropzone ${highlight ? 'highlight' : ''}`}
-        onDragOver={this.onDragOver}
-        onDragLeave={this.onDragLeave}
-        onDrop={this.onDrop}
-      >
-        <input
-          ref={this.fileInputRef}
-          className="fileInput"
-          type="file"
-        />
-        {noFileName ? (
-          <p>Drop file to upload</p>
-        ) : (
-          <p>{fileName}</p>
-        )}
+      <div>
+        <div
+          className={`dropzone ${highlight ? 'highlight' : ''}`}
+          onDragOver={this.onDragOver}
+          onDragLeave={this.onDragLeave}
+          onDrop={this.onDrop}
+        >
+          <input
+            ref={this.fileInputRef}
+            className="fileInput"
+            type="file"
+          />
+          {noFileName ? (
+            <p className="uploadFile">Drop file to upload</p>
+          ) : (
+            <p className="uploadFile">{fileName}</p>
+          )}
+        </div>
+        <div>
+          {message ? (
+            <p className="uploadSuccess">{message}</p>
+          ) : ''
+          }
+        </div>
       </div>
     );
   }
